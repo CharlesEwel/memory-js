60345b74a186e598748a384f77e40e0873239fb3
Charles Ewel and Leah Nelson <charles@>
2016-08-01 14:03:07 -0700

    remove console logs

--- js/memory-interface.js
+++ js/memory-interface.js
@@ -13,10 +13,6 @@ $(function() {
     $(this).find(".face-up").show();
     $("#turn").text(parseInt(newGame.turn/2)+1);
     var match = newGame.processTurn($(this).attr('card'), $(this).attr('slot'));
-    console.log(newGame.turn);
-    console.log(match);
-    console.log(newGame.previousCardId);
-    console.log(newGame.previousSlotId);
     if (match === false) {
       $(this).find(".face-up").hide(1000);
       $(this).find(".face-down").show(1000);
